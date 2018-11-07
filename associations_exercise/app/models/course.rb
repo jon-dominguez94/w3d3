f6ceeb60d27164e8f297ce511e8be3ee8bbec26e
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-07 14:09:03 -0800

    Create enrollments and enrolled_students

--- associations_exercise/app/models/course.rb
+++ associations_exercise/app/models/course.rb
@@ -17,6 +17,8 @@ class Course < ApplicationRecord
     foreign_key: :course_id,
     class_name: :Enrollment
 
-  
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
 
 end
