0d420e803271110042a072fdbc40c87205a02d12
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-07 17:27:16 -0800

    Write Visit table migration

--- url_shortener/URLShortener/db/migrate/20181108012342_create_visits.rb
+++ url_shortener/URLShortener/db/migrate/20181108012342_create_visits.rb
@@ -1,8 +1,12 @@
 class CreateVisits < ActiveRecord::Migration[5.1]
   def change
     create_table :visits do |t|
-
+      t.integer :user_id, null: false, unique: true
+      t.integer :url_id, null: false, unique: true
       t.timestamps
     end
+
+    add_index :visits, :user_id
+    add_index :visits, :url_id
   end
 end
